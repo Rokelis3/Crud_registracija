7aa5337d8df3919c8e1eed8fd10db0fa3cfcda0c
Rokelis3 <rokasstankevicius3@gmail.com>
2022-05-03 21:03:59 +0300

    update

--- src/app/models/clubRegistration.ts
+++ src/app/models/clubRegistration.ts
@@ -1,8 +1,14 @@
 export interface ClubRegistation{
     alergies:Array<string>[],
-    club:Array<any>[],
+    club:Array<{
+        year:number,
+        clubName:string,
+        typeOfClub:string
+    }>,
     email:string,
     klass:number,
     name:string,
     surname:string
 }
+
+   
